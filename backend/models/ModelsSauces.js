041d360fe0b410139be3d605bc56eadefe93c0c3
luk3s <luk3s.perzival@gmail.com>
2023-01-16 16:38:04 +0100

    WIP 2

--- backend/models/ModelsSauces.js
+++ backend/models/ModelsSauces.js
@@ -6,13 +6,13 @@ let sauceSchema = mongoose.Schema({
     name: { type: String, require: true},
     manufacturer: { type: String, require: true},
     description: { type: String, require: true },
-    mainPepper: { type: string, require: true },
+    mainPepper: { type: String, require: true },
     imageUrl: { type: String, require: true },
     heat: { type: Number, require: true },
     likes: { type: Number, require: true },
     dislikes: { type: Number, require: true },
-    userLiked: { type: [string], require: true },
-    userDisliked: { type: [string], require: true }
+    userLiked: { type: [String], require: true },
+    userDisliked: { type: [String], require: true }
 });
 
 //export du schéma en tant que modèle Mongoose et le rendant disponible sur app express

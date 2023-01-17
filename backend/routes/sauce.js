f16a0dc4114a22745c334e1e4e2ec03113124b3a
luk3s <luk3s.perzival@gmail.com>
2023-01-17 15:21:09 +0100

    WIP 9

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -2,10 +2,11 @@ const express = require('express');
 let router = express.Router();
 
 let auth = require('../middleware/auth');
+let multer = require('../middleware/multer-config');
 let stuffCtrl = require('../controllers/sauce');
 
-router.post('/', auth, stuffCtrl.createSauce);
-router.put('/:id', auth, stuffCtrl.modifySauce);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
 router.delete('/:id', auth, stuffCtrl.deleteSauce);
 router.get('/:id', auth, stuffCtrl.getOneSauce);
 router.get('/' + '', auth, stuffCtrl.getAllSauce);

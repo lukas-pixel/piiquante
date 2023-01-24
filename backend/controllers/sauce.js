ba6030cce4bc849877036aa55fde72a9b90ebb61
luk3s <luk3s.perzival@gmail.com>
2023-01-24 14:25:58 +0100

    WIP 15

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -72,12 +72,14 @@ exports.deleteSauce = (req, res) => {
         .catch((error) => res.status(500).json({ error }));
 };
 
+////////// FONCTION POUR CHOISIR UNE SAUCE //////////
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(( sauce ) => res.status(200).json( sauce ))
         .catch(( error ) => res.status(404).json({ error: error }));
 };
 
+////////// FONCTION POUR CHOISIR PLUSIEURS SAUCE //////////
 exports.getAllSauce = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,17 +4,15 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         // recuperation du token dans le header de la requête d'authorization
-        let token = req.headers.authorization.split('')[1];
+        let token = req.headers.authorization.split(" ")[1];
         // vérifie le token avec la clé secrète
         let decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         // vérifie si le token dans le header authorization est le même que celui de l'utilisateur
         let userId = decodedToken.userId;
-        if (req.body.userId && req.body.userId !== userId) {
-            throw 'Utilisateur non autorisé';
-        } else {
-            // si tout est bon on execute le middleware suivant en fonction de l'action utilisateur
-            next();
-        }
+        req.auth = {
+            userId: userId
+        };
+        next();
     } catch {
         // repond une erreur 401 si probleme d'authentification
         res.status(401).json({
